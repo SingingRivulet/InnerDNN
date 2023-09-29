760fd4ec6b2f6b689d9381038f6ece977116f144
cgoxop <cgoxopx@qq.com>
2023-09-29 23:52:42 +0800

    model

---
+++ include/innerDNN/model_gpt.h
@@ -0,0 +1,5 @@
+#ifndef INNER_DNN_MODEL_GPT
+#define INNER_DNN_MODEL_GPT
+#include "shaders.h"
+
+#endif
---
+++ include/innerDNN/model_rwkv.h
@@ -0,0 +1,5 @@
+#ifndef INNER_DNN_MODEL_RWKV
+#define INNER_DNN_MODEL_RWKV
+#include "shaders.h"
+
+#endif
